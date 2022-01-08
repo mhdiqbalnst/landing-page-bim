452659a24797c5a47579c712a78a280aef6418eb
mhdiqbalnst <muhammadiqbal121994@gmail.com>
2022-01-09 01:21:10 +0700

    Create Navbar and Layout Home

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,107 +1,25 @@
-import  { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import logo from '../../images/logopng.png';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import CallOutlinedIcon from '@mui/icons-material/CallOutlined';
+import BallotOutlinedIcon from '@mui/icons-material/BallotOutlined';
 import './navbar.scss';
 
-const pages = ['Beranda', 'Konsesi', 'Papan Iklan', 'Kontak'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
 const ResponsiveAppBar = () => {
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  return (
-    <AppBar position="static">
-      <Container maxWidth="lg">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-          >
-            <img src={logo} alt="logo-bim" width={150} />
-          </Typography>
   
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-          <img src={logo} alt="logo-bim" />
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-              
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+  return (<>
+    <div className='c-navbar'>
+      <div className='p-navbar'>
+        <div className='icon-home'>
+          <HomeRoundedIcon />
+        </div>
+        <div className='icon-list'>
+          <BallotOutlinedIcon />
+        </div>
+        <div className='icon-phone'>
+          <CallOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  </>);
 };
+
 export default ResponsiveAppBar;
--- src/view/home/index.jsx
+++ src/view/home/index.jsx
@@ -1,16 +1,149 @@
 import './home.scss';
+import { useState } from 'react';
 import Navbar from '../../components/navbar';
-import home_img from '../../images/web1.jpg'
+import logo from '../../images/logopng2.png';
+import MyLocationOutlinedIcon from '@mui/icons-material/MyLocationOutlined';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slide from '@mui/material/Slide';
 
 const Home = () => {
-  return(<>
-  <Navbar />
-  <div className='c-welcome' style={{ backgroundColor : "red",width : "100%", height : "100vh" }}>
+ const [scroll, setScroll] = useState(false);
+
+//  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  const box = document.getElementById('box-cards');
+
+  if ( box.scrollTop > 0) {
+    setScroll(true);
+    document.getElementById('c-info').style.display = "none";
+    document.getElementById('c-home').style.overflowY = "hidden";
     
-    <div className='img-top'></div>
-  </div>
-  
-  <div style={{ backgroundColor : "blue",width : "100%", height : "100vh" }}></div>
+    document.getElementById('p-home').style.overflowY = "hidden";
+  } else {
+    setScroll(false);
+    document.getElementById('c-info').style.display = "inline-block";
+    document.getElementById('c-home').style.overflowY = "hidden";
+    box.style.overflowY = "hidden";
+    document.getElementById('p-home').style.overflowY = "hidden";
+  }
+}
+
+  return(<>
+    <Navbar />
+    <div id="c-home" className='c-home'> 
+      <div id="p-welcome" className='p-welcome'>
+        <div className='img-welcome'>
+          <span>
+            <MyLocationOutlinedIcon />
+            <p>Bandara Internasional Minangkabau</p>
+          </span>
+          <img src={logo} alt="" />
+        </div>  
+        <div className='text-welcome'>
+          <h3>Temukan tempat terbaik untuk usaha anda</h3>
+        </div>
+      </div>
+      <div id='c-info' className='c-info'>
+        <div className='p-info'></div>
+      </div>
+      {scroll &&  
+      <Slide direction="up" in={true} 
+          style={{ transformOrigin: '0 0 0' }}
+          {...(true ? { timeout: 700 } : {})}
+        >
+       
+      <div id="p-home" className='p-home' 
+        style={{ 
+          top : "85px",
+          height : "85vh",
+          overflowY : "auto",
+          borderTopLeftRadius : "15px",
+          borderTopRightRadius : "15px", 
+          paddingTop : "50px"
+        }}>
+            <div id="box-cards" className='box-card' onScroll={()=> scrollFunction()}>
+          <h6>Ruangan Tersedia</h6>
+          <div className='slide-card'>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+          </div>
+
+          <h6>Papan Iklan Tersedia</h6>
+          <div className='slide-card'>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+          </div>
+
+          <h6>Papan Iklan Tersedia</h6>
+          <div className='slide-card'>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+          </div>
+        </div> 
+      </div> 
+      </Slide>
+      }
+      {!scroll && <div id="p-home" className='p-home' >
+        <div id="box-cards" className='box-card' onScroll={()=> scrollFunction()}>
+          <h6>Ruangan Tersedia</h6>
+          <div className='slide-card'>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+          </div>
+
+          <h6>Papan Iklan Tersedia</h6>
+          <div className='slide-card'>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+          </div>
+
+          <h6>Papan Iklan Tersedia</h6>
+          <div className='slide-card'>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+            <div className='cards'></div>
+          </div>
+        </div>  
+      </div>}
+      
+    </div>
   </>)
 }
 
